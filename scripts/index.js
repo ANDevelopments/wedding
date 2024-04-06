afb84ba581349364615f8055896d882856e74b59
ANDevelopments <HeavyMetalSoldier@yandex.ru>
2024-04-07 02:51:45 +0500

    осталась косметика

--- scripts/index.js
+++ scripts/index.js
@@ -36,17 +36,60 @@ const initFadeInAnimation = () => {
     });
 }
 
+const helpers = {
+  mm: undefined,
+  sumArrayItems: (array, itemsToSum) => {
+    return array.slice(0, itemsToSum).reduce((acc, val) => acc + val, 0)
+  },
+}
+
+const pinSameHeightSections = () => {  
+  document.querySelectorAll('.section--pinned').forEach((section) => {
+    const itemsAmount = section.querySelectorAll('.item').length
+    const itemHeight = (section.querySelectorAll('.item')[0].offsetHeight / window.innerHeight) * 100
+    const overlap = 5
+    const padding = 9
+    const endValue = itemHeight + ((itemsAmount - 1) * overlap) + (padding * 2)
+
+    ScrollTrigger.create({
+      end: `100% ${endValue}%`,
+      endTrigger: section,
+      pin: true,
+      pinSpacing: false,
+      start: `0 ${padding}%`,
+      trigger: section.querySelector('.column--left > div'),
+    })
+
+    ScrollTrigger.create({
+      end: `100% ${endValue}%`,
+      endTrigger: section,
+      pin: true,
+      pinSpacing: false,
+      start: `0 ${padding}%`,
+      trigger: section.querySelector('.column--center > div'),
+    })
+
+    section.querySelectorAll('.item').forEach((item, index) => {
+      ScrollTrigger.create({
+        end: `100% ${endValue}%`,
+        endTrigger: section,
+        onEnter: () => helpers.setMarker(section, index),
+        onLeaveBack: () => helpers.setMarker(section, index === 0 ? index : index - 1),
+        pin: true,
+        pinSpacing: false,
+        start: `0 ${padding + index * overlap}%`,
+        trigger: item,
+      })
+    })
+  })
+}
+
 const hero = {
     element: document.querySelector('.hero'),
     titles: document.querySelectorAll('.hero_title_row_text'),
     media: document.querySelectorAll('.hero_media'),
     mediaImages: document.querySelectorAll('.hero_media_image')
 };
-
-function playAudio () {
-    var audio = new Audio('./music/RADWIMPS-Sparkle.mp3');
-    audio.play();
-}
 
 const init = ()=> {
     gsap.set(hero.titles, {autoAlpha: 0, yPercent: -101});
@@ -56,10 +99,11 @@ const init = ()=> {
     animateHero();
     initFadeInAnimation();
     updateCountdown();
+    pinSameHeightSections();
 }
 
 const animateHero = ()=> {
-    const tl = gsap.timeline({defaults:{duration: 2, ease: 'expo.inOut'}});
+    const tl = gsap.timeline({defaults:{duration: 5, ease: 'expo.inOut'}});
     tl.to(hero.media, {xPercent: 0, autoAlpha: 1}, 0)
     .to(hero.mediaImages, {xPercent: 0, stagger: 0.016}, 0.16)
     .to(hero.titles, {autoAlpha: 1, yPercent: 0, stagger: 0.016}, 2)
